54caeb67e972849b1aca7d56cf2ce94c8ec8297e
charith-codex <pvm.charith@gmail.com>
2024-03-26 23:26:24 +0530

    version 1.1

---
+++ script.js
@@ -0,0 +1,3 @@
+let api =`https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
+const fromDropDown = document.getElementById('from-currency-select');
+const toDropDown = document.getElementById('to-currency-select');
